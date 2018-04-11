b0e6792528ca0e7924a193074fc945c4696a83ea
Shihab EK <sekonlive@gmail.com>
2018-04-11 23:12:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,25 @@
-<?php
-ini_set("allow_url_open", 1);
-$response = file_get_contents("php://input");
-$update = json_decode($response, true);
-function processMessage()
-{
-$response = file_get_contents("php://input");
-$update = json_decode($response, true);
- $actionName=$update["result"]["action"];
-   
-    $pnr = $update["result"]["parameters"]["PNR"];
-    $pax = $update["result"]["parameters"]["NoPax"];
-    $date = $update["result"]["parameters"]["date"];
-    $Airline = $update["result"]["parameters"]["Airline"];
-    $Client = $update["result"]["parameters"]["Client"];
-    $status = "Done";
-    switch($actionName)
-    {
-        
-           case 'AddEntry'   : 
-                          
-                        
-//                        $getStatusUrl="http://manage.otb-network.com/application/GETProcessing.php?PNR=".$pnr."&airline=".$Airline."&no_pax=".$pax."&client=".$Client."&status=".$status."&date=".$date;
-//                        $content=file_get_contents($getStatusUrl);
-//                        $Obj=json_decode($content, true);
-//                        $StatusCode=$Obj['StatusCode'];
-//                        if($StatusCode=="200"){
-//                            $speech = "Done \n ---------- \n  Airline: ".$Airline." \n PNR: ".$pnr."\n Pax: ".$pax."\n Client: ".$Client;
-//                            $displayText = "Done \n ---------- \n  Airline: ".$Airline." \n PNR: ".$pnr."\n Pax: ".$pax."\n Client: ".$Client;
-//                        }else{
-//                            $speech = "Failed \n ---------- \n  Airline: ".$Airline." \n PNR: ".$pnr."\n Pax: ".$pax."\n Client: ".$Client;
-//                            $displayText = "Failed \n ---------- \n  Airline: ".$Airline." \n PNR: ".$pnr."\n Pax: ".$pax."\n Client: ".$Client;
-//                        }
-            $speech = "TEST";
-            $displayText = "Test";
-                        $source = "OTBNetwork";
-                        break;
-     
-            default :   $speech = $city." Something went wrong ! Try again...";
-                        $displayText = $actionName."Something went wrong ! Try again...";
-                        $source = "DGO-Server";
-                        break;
+<?php 
+
+function processMessage($update) {
+    if($update["result"]["action"] == "AddEntry"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
-    
-    sendMessage(array("source"=>$source,"speech"=>$speech,"displayText"=>$displayText,"contextOut"=> array()));
-                        
-    
-      
-    
 }
-function sendMessage($parameters)
-{
-    header('Content-type: application/json');
+ 
+function sendMessage($parameters) {
     echo json_encode($parameters);
 }
-if (isset($update["result"]["action"]))
-{
-     processMessage();
+ 
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
 }
-?>
 
+?>
     © 2018 GitHub, Inc.
