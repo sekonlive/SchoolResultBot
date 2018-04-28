93b4d76a0a10e42e848597da41f3c01ea2a043a6
Shihab EK <sekonlive@gmail.com>
2018-04-28 16:14:30 +0530

    ed

--- index.php
+++ index.php
@@ -427,10 +427,9 @@ function processMessage($update) {
                                 $STD = ReternZero($Obj["STD"]);
                                 $LTA = ReternZero($Obj["LTA"]);
                                 $fDate = date("d M y", strtotime($fDate));
-                                $Expanded=array();
                                 $EC=count($Obj["Data"]);
                                 for($i=0;$i<$EC;$i++)
-                                    $Expanded+="*".$Obj["Data"][i]["Name"]."*\n*LTD:* ".$Obj["Data"][i]["LTD"]."*\n*LTA:* ".$Obj["Data"][i]["LTA"]."*\n*STD:* ".$Obj["Data"][i]["STD"];
+                                    $Expanded+="*".$Obj["Data"][i]["Name"]."*\n*LTD:* ".$Obj["Data"][i]["LTD"]."*\n*LTA:* ".$Obj["Data"][i]["LTA"]."*\n*STD:* ".$Obj["Data"][i]["STD"]."\n\n";
                                 $Success = array("speech" => " DSR - VISA \n ----------- \n Date: ".$fDate."\n*Long Term DXB:*  ".$LTD."\n*Long Term AUH:* ".$LTA."\n*Short Term DXB: ".$STD,
                                 "type" => 0);
                                 $TSuccess = array("title" => "DSR - VISA",
